510692e4c4f5ac14a283bb8bde8a6ede34a22a93
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-01 11:58:15 +0530

    EXCEPTION-HANDLING

---
+++ Exception.java
@@ -0,0 +1,9 @@
+package ExceptionUserRegistration;
+
+public class Exception 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "EXCEPTION HANDLING BASED PROGRAMS " );
+    }
+}
